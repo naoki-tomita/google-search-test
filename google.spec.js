99857e894a250be97fd043d98e7f1ab08bbd0d8e
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2018-08-07 20:12:04 +0900

    Add comment.

--- google.spec.js
+++ google.spec.js
@@ -3,10 +3,15 @@ const { GoogleTop } = require("./pages/google");
 
 describe("Google", () => {
   it("should search RICOH IT SOLUTIONS", async () => {
+    // This parameter must be true if Non Angular app testing.
     browser.ignoreSynchronization = true;
+
     const topPage = new GoogleTop();
+    // Visit function is means show page.
     await topPage.visit();
     await topPage.inputSearchText("RICOH IT SOLUTIONS");
+    // Generally, Page object returns other page object when page transition has occurring.
+    // Executeing search returns search result page.
     const searchResultPage = await topPage.search();
 
     const searchResults = await searchResultPage.getSearchResults();
